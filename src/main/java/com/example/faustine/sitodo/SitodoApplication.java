9970e9a8a86f0dc538ce08d192c3019b21853b19
Faustine Ben Axel Ompusunggu <faustinebenaxel08@gmail.com>
2022-09-28 09:46:54 +0700

    initial commit

---
+++ src/main/java/com/example/faustine/sitodo/SitodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.faustine.sitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitodoApplication.class, args);
+	}
+
+}
